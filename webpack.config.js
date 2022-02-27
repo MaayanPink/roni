bed6d5259213dce9301b4d15b1d6cca62462bab7
MaayanPink <94897069+MaayanPink@users.noreply.github.com>
2022-02-27 08:20:58 +0200

    webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    devtool: 'eval-source-map',
+    devtool: 'source-map',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
